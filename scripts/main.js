20bef44a5e36ba2fece9a4838d8c70da583a6664
2y2son4 <adrianadelteso@gmail.com>
2021-01-28 09:12:48 +0100

    Write first innerHTML function for the handler function

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ function getRandomNumber(max) {
 	return Math.ceil(Math.random() * max);
 }
 
+const randomNumber = getRandomNumber(100);
+
 console.log(getRandomNumber(100));
 
 const inputElement = document.querySelector('.js-input-number');
@@ -11,9 +13,18 @@ const paragraph1Element = document.querySelector('.js-paragraph1');
 const paragraph2Element = document.querySelector('.js-paragraph2');
 const btnElement = document.querySelector('.js-btn');
 
-function btnHandler () {
-    
+function paragraph1Preview (clue) {
+    return paragraph1Element.innerHTML = clue;
+}
+
+/*function btnHandler () {
+    const inputValue = parseInt(inputElement.value);
+
+    if () {
+
+    }
+
 }
 
 
-btnElement.addEventListener('click', btnHandler);
+btnElement.addEventListener('click', btnHandler);*/
