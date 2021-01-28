c72d1c07cbd00c58dd2192b76e216b1f34a5b351
2y2son4 <adrianadelteso@gmail.com>
2021-01-28 09:05:35 +0100

    Add cqs

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
-'use strict';
+"use strict";
+
+function getRandomNumber(max) {
+	return Math.ceil(Math.random() * max);
+}
+
+console.log(getRandomNumber(100));
+
+const inputElement = document.querySelector('.js-input-number');
+const paragraph1Element = document.querySelector('.js-paragraph1');
+const paragraph2Element = document.querySelector('.js-paragraph2');
+const btnElement = document.querySelector('.js-btn');
+
+function btnHandler () {
+    
+}
+
+
+btnElement.addEventListener('click', btnHandler);
