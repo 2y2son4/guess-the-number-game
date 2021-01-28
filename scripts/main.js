d34f31f5dc3b820ca1c20ddce6d361e792de6a46
2y2son4 <adrianadelteso@gmail.com>
2021-01-28 11:00:00 +0100

    Fix numbers' range in conditional

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,12 @@
-'use strict';
+"use strict";
 
-const inputElement = document.querySelector('.js-input-number');
-const paragraph1Element = document.querySelector('.js-paragraph1');
-const paragraph2Element = document.querySelector('.js-paragraph2');
-const btnElement = document.querySelector('.js-btn');
-
-
-
+const inputElement = document.querySelector(".js-input-number");
+const paragraph1Element = document.querySelector(".js-paragraph1");
+const paragraph2Element = document.querySelector(".js-paragraph2");
+const btnElement = document.querySelector(".js-btn");
 
 function paragraph1Clue(clue) {
-    return paragraph1Element.innerHTML = clue;
+	return (paragraph1Element.innerHTML = clue);
 }
 
 function getRandomNumber(max) {
@@ -20,26 +17,25 @@ const randomNumber = getRandomNumber(100);
 
 console.log(randomNumber);
 
-function btnHandler () {
-    const inputValue = parseInt(inputElement.value);
+function btnHandler() {
+	const inputValue = parseInt(inputElement.value);
 
-    if (inputValue < randomNumber && inputValue > 0) {
-        paragraph1Clue('El número es demasiado bajo.');
-    } else if (inputValue > randomNumber && inputValue < 100) {
-        paragraph1Clue('El número es demasiado alto.');
-    } else if (inputValue === randomNumber) {
-        paragraph1Clue('¡¡Has ganado, campeona!!');
-    } else {
-        paragraph1Clue('El número debe estar entre 1 y 100');
-    }
+	if (inputValue < randomNumber && inputValue > 0) {
+		paragraph1Clue("El número es demasiado bajo.");
+	} else if (inputValue > randomNumber && inputValue <= 100) {
+		paragraph1Clue("El número es demasiado alto.");
+	} else if (inputValue === randomNumber) {
+		paragraph1Clue("¡¡Has ganado, campeona!! 🤡");
+	} else {
+		paragraph1Clue("El número debe estar entre 1 y 100.");
+	}
 }
 
-btnElement.addEventListener('click', btnHandler);
+btnElement.addEventListener("click", btnHandler);
 
-let clicksCount = 0; 
-function attemptHandler () {    
-      
-    return paragraph2Element.innerHTML = `Número de intentos: ${clicksCount++}`;
+let clicksCount = 0;
+function attemptHandler() {
+	return (paragraph2Element.innerHTML = `Número de intentos: ${clicksCount++}`);
 }
 
-btnElement.addEventListener('click', attemptHandler);
+btnElement.addEventListener("click", attemptHandler);
